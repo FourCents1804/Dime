ad5dbb4ce141f68278e6b51d23a95c750f2b52a0
archon21 <rwetmore3@gmail.com>
2018-06-19 16:01:37 -0400

    created a react native app from the react native cli as a boilerplate for us

---
+++ App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Jenny is cool!</Text>
+        <Text>Jay is a Hyung</Text>
+        <Text>Yacine the machine</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
