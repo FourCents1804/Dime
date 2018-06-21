5a2ecc5fbea1262519d1d686a427fb008135ab32
Yacine Benzerga <benzerga.yacine@hotmail.com>
2018-06-21 16:12:43 -0400

    feat(User seedfile)

---
+++ server/seedFile/seed.js
@@ -0,0 +1,36 @@
+'use strict';
+
+const { db } = require('../db');
+const { User } = require('../db/models');
+const userData = require('./userMock');
+
+async function seed() {
+  await db.sync({ force: true });
+  console.log('db synced!');
+
+  const userPromises = [];
+  for (let i = 0; i < userData.length; i++) {
+    let newUser = User.create(userData[i]);
+    await userPromises.push(newUser);
+  }
+  await Promise.all(userPromises);
+
+  console.log(`seeded ${userData.length} users`);
+  console.log(`seeded successfully`);
+}
+
+if (module === require.main) {
+  seed()
+    .catch(err => {
+      console.error(err);
+      process.exitCode = 1;
+    })
+    .then(() => {
+      console.log('closing db connection');
+      db.close();
+      console.log('db connection closed');
+    });
+  console.log('seeding...');
+}
+
+module.exports = seed;
