2aa5eff6e27bedd7632c596985dc09ebb9adce79
Jay.Lee <kkomarocker@gmail.com>
2018-06-26 12:33:57 -0400

    feat(Plus Button): Plus button in home screen now have additional functionality

--- App/components/Home.js
+++ App/components/Home.js
@@ -17,23 +17,25 @@ import styles from "../../public";
 import React, { Component } from "react";
 import Histogram from "../D3/Histogram";
 import Line from "../D3/Line";
+import ActionButton from "react-native-action-button";
+import Icon from "react-native-vector-icons/Ionicons";
 
 const Home = props => {
   const chartWidth = 250;
   const chartHeight = 250;
   const { user, navigate } = props;
   return (
-    <View style={styles.container}>
-      <ScrollView>
-        <Header
-          placement="left"
-          leftComponent={{ icon: "menu", color: "#fff", size: 35 }}
-          centerComponent={{
-            text: user.firstName + " " + user.lastName,
-            style: { fontSize: 25, color: "#fff" }
-          }}
-          rightComponent={{ icon: "person", color: "#fff", size: 35 }}
-        />
+    <ScrollView>
+      <Header
+        placement="left"
+        leftComponent={{ icon: "menu", color: "#fff", size: 35 }}
+        centerComponent={{
+          text: user.firstName + " " + user.lastName,
+          style: { fontSize: 25, color: "#fff" }
+        }}
+        rightComponent={{ icon: "person", color: "#fff", size: 35 }}
+      />
+      <View style={{ backgroundColor: "#fff" }}>
         <Text>Welcome, {user.firstName} </Text>
         <Pie userPurchases={userPurchases} />
         <RNSCHistogram
@@ -44,20 +46,31 @@ const Home = props => {
         <Histogram width={250} height={200} margin={20} />
         <Line />
         <Divider style={styles.dividerS} />
-        <TouchableOpacity
-          style={{
-            alignSelf: "flex-end",
-            alignItems: "center"
-          }}
-          onPress={() => navigate("Webcam")}
-        >
-          <Image
-            style={{ height: 70, width: 70, marginBottom: 25 }}
-            source={require("../../public/plus.png")}
-          />
-        </TouchableOpacity>
-      </ScrollView>
-    </View>
+        <ActionButton buttonColor="rgba(231,76,60,1)">
+          <ActionButton.Item
+            buttonColor="#9b59b6"
+            title="New Task"
+            onPress={() => console.log("notes tapped!")}
+          >
+            <Icon name="md-create" style={styles.actionButtonIcon} />
+          </ActionButton.Item>
+          <ActionButton.Item
+            buttonColor="#3498db"
+            title="Notifications"
+            onPress={() => {}}
+          >
+            <Icon name="md-notifications-off" style={styles.actionButtonIcon} />
+          </ActionButton.Item>
+          <ActionButton.Item
+            buttonColor="#1abc9c"
+            title="All Tasks"
+            onPress={() => {}}
+          >
+            <Icon name="md-done-all" style={styles.actionButtonIcon} />
+          </ActionButton.Item>
+        </ActionButton>
+      </View>
+    </ScrollView>
   );
 };
 
