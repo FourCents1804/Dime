d09eda39956149377c17944262920684d7a7ab8c
Jay.Lee <kkomarocker@gmail.com>
2018-06-28 14:48:18 -0400

    feat(past spend): category for month added

--- App/Root.js
+++ App/Root.js
@@ -1,11 +1,11 @@
-import React from 'react';
-import {  View } from 'react-native';
-import {  me } from './store/Thunks/User';
-import { Home, Login, Navigation, Menu } from './components';
-import { connect } from 'react-redux';
-import Firebase from './components/Firebase/Firebase';
-import styles from '../public';
-import Drawer from 'react-native-drawer';
+import React from "react";
+import { View } from "react-native";
+import { me } from "./store/Thunks/User";
+import { Home, Login, Navigation, Menu } from "./components";
+import { connect } from "react-redux";
+import Firebase from "./components/Firebase/Firebase";
+import styles from "../public";
+import Drawer from "react-native-drawer";
 
 class Root extends React.Component {
   state = {
@@ -30,11 +30,33 @@ class Root extends React.Component {
 
   render() {
     const drawerStyles = {
-      drawer: { shadowColor: '#000000', shadowOpacity: 0.8, shadowRadius: 3 },
+      drawer: { shadowColor: "#000000", shadowOpacity: 0.8, shadowRadius: 3 },
       main: { paddingLeft: 3 }
     };
     const { navigate } = this.props.navigation;
-    return this.state.isLoggedIn ? (
+    // return this.state.isLoggedIn ? (
+    //   <Drawer
+    //     ref={ref => (this._drawer = ref)}
+    //     type="displace"
+    //     content={<Menu navigate={navigate} />}
+    //     tapToClose={true}
+    //     openDrawerOffset={0.3}
+    //     panCloseMask={0.2}
+    //     closedDrawerOffset={-3}
+    //     styles={drawerStyles}
+    //     tweenHandler={ratio => ({
+    //       main: { opacity: (2 - ratio) / 2 }
+    //     })}
+    //   >
+    //     <View style={{ flex: 1 }}>
+    //       <Navigation navigate={navigate} openMenu={this.openMenu} />
+    //       <Home navigate={navigate} />
+    //     </View>
+    //   </Drawer>
+    // ) : (
+    //   <Login navigate={navigate} />
+    // );
+    return (
       <Drawer
         ref={ref => (this._drawer = ref)}
         type="displace"
@@ -53,29 +75,7 @@ class Root extends React.Component {
           <Home navigate={navigate} />
         </View>
       </Drawer>
-    ) : (
-      <Login navigate={navigate} />
     );
-    // return (
-    //   <Drawer
-    //   ref={ref => (this._drawer = ref)}
-    //   type="displace"
-    //   content={<Menu navigate={navigate} />}
-    //   tapToClose={true}
-    //   openDrawerOffset={0.3}
-    //   panCloseMask={0.2}
-    //   closedDrawerOffset={-3}
-    //   styles={drawerStyles}
-    //   tweenHandler={ratio => ({
-    //     main: { opacity: (2 - ratio) / 2 }
-    //   })}
-    // >
-    //   <View style={{ flex: 1 }}>
-    //     <Navigation navigate={navigate} openMenu={this.openMenu} />
-    //     <Home navigate={navigate} />
-    //   </View>
-    //   </Drawer>
-
   }
 }
 
--- App/components/SpendHistory.js
+++ App/components/SpendHistory.js
@@ -1,43 +1,71 @@
 import React from "react";
-import { ScrollView, Text } from "react-native";
+import { ScrollView, Text, Picker, View } from "react-native";
+import { Dropdown } from "react-native-material-dropdown";
 import { Card } from "react-native-elements";
 import { sanFranciscoWeights } from "react-native-typography";
 import styles from "../../public";
+import * as d3 from "d3";
 
 const purchases = require("../../seed/purchaseData");
 
-const spendHistory = purchaseData => {
-  const firstTen = [];
-  for (let i = 0; i < 10; i++) {
-    firstTen.push(purchaseData[i]);
+const formatter = d3.timeFormat("%b %y");
+const parser = d3.timeParse("%b %y");
+
+const month = d3
+  .nest()
+  .key(mon => formatter(new Date(mon.createdAt)))
+  .entries(purchases);
+
+const monthArr = month.map(eachMon => {
+  let valueObj = {};
+  if (!valueObj["value"]) {
+    valueObj["value"] = eachMon.key;
   }
-  return firstTen;
+  return valueObj;
+});
+
+let categoryArr = [];
+
+const categoryForSelectedVal = val => {
+  let month = "";
+  if (
+    parser(val)
+      .getMonth()
+      .toString().length === 2
+  ) {
+    month = `${parser(val).getFullYear()}-${parser(val).getMonth() + 1}`;
+  } else {
+    month = `${parser(val).getFullYear()}-0${parser(val).getMonth() + 1}`;
+  }
+
+  purchases.forEach(eachPurchase => {
+    if (eachPurchase.createdAt.slice(0, 7) === month) {
+      categoryArr.push({ value: eachPurchase.categoryBroad });
+    }
+  });
+
+  return categoryArr;
 };
 
 const SpendHistory = props => {
-  console.log(spendHistory(purchases));
   return (
-    <ScrollView>
-      {spendHistory(purchases).map(eachPurchase => {
-        return (
-          <Card
-            key={eachPurchase.transaction_id}
-            title={eachPurchase.name}
-            titleStyle={sanFranciscoWeights.medium}
-          >
-            <Text style={[sanFranciscoWeights.light, styles.faqContent]}>
-              Date: {eachPurchase.createdAt}
-            </Text>
-            <Text style={[sanFranciscoWeights.light, styles.faqContent]}>
-              Amount: ${eachPurchase.amount}
-            </Text>
-            <Text style={[sanFranciscoWeights.light, styles.faqContent]}>
-              Category: {eachPurchase.categoryDetailed}
-            </Text>
-          </Card>
-        );
-      })}
-    </ScrollView>
+    <View>
+      <View>
+        <Dropdown
+          label="Select Month"
+          data={monthArr}
+          style={sanFranciscoWeights.light}
+          onChangeText={value => categoryForSelectedVal(value)}
+        />
+      </View>
+      <View>
+        <Dropdown
+          label="Select Category"
+          data={categoryArr}
+          style={sanFranciscoWeights.light}
+        />
+      </View>
+    </View>
   );
 };
 
