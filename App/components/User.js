f8caaef227ba6647c840ac0b7522b74ccefca42d
Jenny Wang <jennysihua@gmail.com>
2018-06-28 15:39:54 -0400

    feat(userform edits)

--- App/components/User.js
+++ App/components/User.js
@@ -1,20 +1,50 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import {  View, Text } from 'react-native';
-import  styles from '../../public/index'
+import {  View, Text, Button, StyleSheet } from 'react-native';
+import t from 'tcomb-form-native'
+const Form = t.form.Form;
 
 class User extends Component {
+
+  handleSubmit () {
+    const value = this._form.getValue(); // use that ref to get the form value
+    console.log('value: ', value);
+  }
+
   render() {
-    const { user } = this.props;
+
+    const userForm = t.struct({
+      email: t.String,
+      username: t.String,
+      password: t.String,
+      terms: t.String
+    });
+
     return (
       <View style={styles.container}>
         <Text>
-          {user.firstName}
+          <Form
+          ref={c => this._form = c}
+            type={userForm}
+            />
         </Text>
+        <Button
+          title="Save"
+          onPress={this.handleSubmit}
+        />
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    justifyContent: 'center',
+    marginTop: 50,
+    padding: 20,
+    backgroundColor: '#ffffff',
+  },
+});
 
 const mapStateToProps = state => ({
   user: state.User
