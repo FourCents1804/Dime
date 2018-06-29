1957b3de585174b763be9d2836457fb50d981acb
Yacine Benzerga <benzerga.yacine@hotmail.com>
2018-06-29 17:40:40 -0400

    receipt recognition from heroku

--- App/store/Thunks/Purchase.js
+++ App/store/Thunks/Purchase.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import Expo from 'expo';
-import {key} from '../../../secrets'
+import { key } from '../../../secrets';
 
 const { manifest } = Expo.Constants;
 const ip = manifest.packagerOpts.dev
@@ -16,102 +16,16 @@ export const defaultPurchase = {};
 
 const addPurchase = newPurchase => ({ type: ADD_PURCHASE, newPurchase });
 
-export const addNewPurchase = base64String => async dispatch => {
-
-  const body = {
-    requests: [
-      {
-        image: {
-          content: base64String,
-        },
-        features: [
-          {
-            type: 'TEXT_DETECTION',
-            maxResults: 1,
-          }
-        ]
-      },
-    ],
-  };
-  let responce = await fetch(`https://vision.googleapis.com/v1/images:annotate?key=${key}`, {
-    method: 'POST',
-    headers: {
-      Accept: 'application/json',
-      'Content-Type': 'application/json'
-
-    },
-    body: JSON.stringify(body)
-  })
-  const parsed = await responce.json()
-  const elements = parsed.responses[0].textAnnotations[0].description.split(
-    '\n'
-  );
-  const items = [];
-  const prices = [];
-  elements.forEach(el => {
-    if (el.match(/^[A-Za-z]/i)) {
-      items.push(el);
-    } else if (el.match(/^[0-9]*[.][0-9]/i)) {
-      prices.push(el);
-    }
-  });
-  console.log(items, prices)
-  // const brand = elements[0];
-  // let summary = {};
-  // if (brand === 'Walmart') {
-  //   const TotalPrice = prices[prices.length - 2];
-  //   const subTotalIdx = items.findIndex(el => {
-  //     return el.startsWith('SUBTOTAL');
-  //   });
-  //   const boughtItems = items.slice(6, subTotalIdx);
-  //   const document = {
-  //     content: boughtItems.join(' '),
-  //     type: 'PLAIN_TEXT'
-  //   };
-  //    responce = await fetch(`https://vision.googleapis.com/v1/images:annotate?key=${key}`, {
-  //     method: 'POST',
-  //     headers: {
-  //       Accept: 'application/json',
-  //       'Content-Type': 'application/json'
-
-  //     },
-  //     body: JSON.stringify({
-  //       requests: [
-  //         {
-  //           image: {
-  //             content: base64String,
-  //           },
-  //           features: [
-  //             {
-  //               type: 'TEXT_DETECTION',
-  //               maxResults: 1,
-  //             }
-  //           ]
-  //         },
-  //       ],
-  //     };)
-  //   })
-  //   summary.category = textContent.categories[0].name.slice(1);
-  //   summary.Total = TotalPrice;
-  //   summary.purchasedItems = boughtItems;
-  // }
-
-  // if (brand === 'WHOLE') {
-  //   const NetSalesIdx = items.findIndex(el => {
-  //     return el.startsWith('Net Sales');
-  //   });
-  //   const TotalIdx = elements.findIndex(el => {
-  //     return el.startsWith('Subtotal');
-  //   });
-  //   const TotalPrice = elements[TotalIdx - 1].replace(/\$/g, '');
-  //   const boughtItems = items.slice(5, NetSalesIdx);
-  //   const document = {
-  //     content: boughtItems.join(' '),
-  //     type: 'PLAIN_TEXT'
-  //   };
-  // const {data } = await axios.post(`http://${ip}/api/receiptRecognition`, {base64String});
-
-  // dispatch(addPurchase(data));
+export const addNewPurchase = img => async dispatch => {
+  try {
+    const newPurchase = await axios.post(
+      'https://safe-bastion-55889.herokuapp.com/api/receiptRecognition',
+      { fileName: img }
+    );
+    dispatch(addPurchase(newPurchase));
+  } catch (err) {
+    console.error(err);
+  }
 };
 
 export default function(state = defaultPurchase, action) {
--- secrets.js
+++ secrets.js
@@ -1,10 +1,8 @@
 export const config = {
-    apiKey: 'AIzaSyCkaOIts1TvwRVjmfihtVNutJnrq3DruX4',
-    authDomain: 'dime-d9d45.firebaseapp.com',
-    databaseURL: 'https://dime-d9d45.firebaseio.com',
-    projectId: 'dime-d9d45',
-    storageBucket: 'dime-d9d45.appspot.com',
-    messagingSenderId: '332258031993'
-  };
-
-export const key = 'AIzaSyAjAp4cp8hu-VyA2s89V5bvH7gd9PDpsTI'
+  apiKey: 'AIzaSyCkaOIts1TvwRVjmfihtVNutJnrq3DruX4',
+  authDomain: 'dime-d9d45.firebaseapp.com',
+  databaseURL: 'https://dime-d9d45.firebaseio.com',
+  projectId: 'dime-d9d45',
+  storageBucket: 'dime-d9d45.appspot.com',
+  messagingSenderId: '332258031993'
+};
