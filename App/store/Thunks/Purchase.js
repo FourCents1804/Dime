80310a0dd68b95ffd48099ae1fc0dd2c77c403a8
Yacine Benzerga <benzerga.yacine@hotmail.com>
2018-06-29 18:04:32 -0400

    recognition linked to heroku

--- App/store/Thunks/Purchase.js
+++ App/store/Thunks/Purchase.js
@@ -1,17 +1,4 @@
 import axios from 'axios';
-import Expo from 'expo';
-import { key } from '../../../secrets';
-<<<<<<< HEAD
-
-=======
->>>>>>> master
-const { manifest } = Expo.Constants;
-const ip = manifest.packagerOpts.dev
-  ? manifest.debuggerHost
-      .split(`:`)
-      .shift()
-      .concat(`:19004`)
-  : `localhost:19004`;
 
 export const ADD_PURCHASE = 'ADD_PURCHASE';
 
@@ -19,92 +6,17 @@ export const defaultPurchase = {};
 
 const addPurchase = newPurchase => ({ type: ADD_PURCHASE, newPurchase });
 
-<<<<<<< HEAD
 export const addNewPurchase = img => async dispatch => {
   try {
+    console.log('hittting image');
     const newPurchase = await axios.post(
       'https://safe-bastion-55889.herokuapp.com/api/receiptRecognition',
       { fileName: img }
     );
+    console.log(newPurchase);
     dispatch(addPurchase(newPurchase));
   } catch (err) {
     console.error(err);
-=======
-export const addNewPurchase = base64String => async dispatch => {
-  const body = {
-    requests: [
-      {
-        image: {
-          content: base64String
-        },
-        features: [
-          {
-            type: 'TEXT_DETECTION',
-            maxResults: 1
-          }
-        ]
-      }
-    ]
-  };
-  let response = await fetch(
-    `https://vision.googleapis.com/v1/images:annotate?key=${key}`,
-    {
-      method: 'POST',
-      headers: {
-        Accept: 'application/json',
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(body)
-    }
-  );
-  const parsed = await response.json();
-  const elements = parsed.responses[0].textAnnotations[0].description.split(
-    '\n'
-  );
-  const items = [];
-  const prices = [];
-  elements.forEach(el => {
-    if (el.match(/^[A-Za-z]/i)) {
-      items.push(el);
-    } else if (el.match(/^[0-9]*[.][0-9]/i)) {
-      prices.push(el);
-    }
-  });
-  const brand = elements[0];
-  let summary = {};
-  if (brand === 'Walmart') {
-    const TotalPrice = prices[prices.length - 2];
-    const subTotalIdx = items.findIndex(el => {
-      return el.startsWith('SUBTOTAL');
-    });
-    const boughtItems = items.slice(6, subTotalIdx);
-    const boughtItemsStr = boughtItems.join(' ');
-    let langResponse = await axios.post(`http://${ip}/api/receiptRecognition`, {boughtItemsStr})
-
-    // let elresponse = await fetch(
-    //   `https://language.googleapis.com/v1/documents:classifyText?key=${key}`,
-    //   {
-    //     method: 'POST',
-    //     headers: {
-    //       Accept: 'application/json',
-    //       'Content-Type': 'application/json'
-    //     },
-    //     data: {
-    //       document: {
-    //         type: 'PLAIN_TEXT',
-    //         language: 'EN',
-    //         content: boughtItemsStr
-    //       }
-    //     }
-    //   }
-    // );
-
-    summary.category = langResponse.data[0].categories[0].name.slice(1);
-    summary.Total = TotalPrice;
-    summary.purchasedItems = boughtItems;
-    console.log(summary);
-    dispatch(addPurchase(summary))
->>>>>>> master
   }
 };
 
