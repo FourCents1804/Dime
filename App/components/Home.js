55309dd92652166452ecf7be9cafebdf0cfaa529
Jay.Lee <kkomarocker@gmail.com>
2018-07-03 15:57:05 -0400

    feat(back to home):navigates to home after manual input is successful

--- App/components/Home.js
+++ App/components/Home.js
@@ -1,14 +1,14 @@
-import { ScrollView, View, Text } from 'react-native';
-import { connect } from 'react-redux';
-import Pie from '../D3/Doughnut';
-import { SpendTable } from './';
-import styles from '../../public';
-import React, { Component } from 'react';
-import { Permissions } from 'expo';
-import ActionButton from 'react-native-action-button';
-import Icon from 'react-native-vector-icons/Ionicons';
-import { getUser } from '../store/Thunks/User';
-import User from './Utility/exampleUser';
+import { ScrollView, View, Text } from "react-native";
+import { connect } from "react-redux";
+import Pie from "../D3/Doughnut";
+import { SpendTable } from "./";
+import styles from "../../public";
+import React, { Component } from "react";
+import { Permissions } from "expo";
+import ActionButton from "react-native-action-button";
+import Icon from "react-native-vector-icons/Ionicons";
+import { getUser } from "../store/Thunks/User";
+import User from "./Utility/exampleUser";
 
 class Home extends Component {
   async componentDidMount() {
@@ -22,6 +22,7 @@ class Home extends Component {
   render() {
     const { user, navigate, purchases } = this.props;
     const firstName = user ? `, ${user.firstName}` : ``;
+    console.log(purchases);
     return (
       <View style={styles.homeContainer}>
         <ScrollView style={{ paddingTop: 10 }}>
@@ -36,14 +37,14 @@ class Home extends Component {
           <ActionButton.Item
             buttonColor="#3498db"
             title="Camera"
-            onPress={() => navigate('Webcam')}
+            onPress={() => navigate("Webcam")}
           >
             <Icon name="ios-camera" size={30} />
           </ActionButton.Item>
           <ActionButton.Item
             buttonColor="#3498db"
             title="Keyboard"
-            onPress={() => navigate('Purchase')}
+            onPress={() => navigate("Purchase")}
           >
             <Icon name="ios-keypad" size={30} />
           </ActionButton.Item>
@@ -56,9 +57,9 @@ class Home extends Component {
 
 const mapStateToProps = state => {
   return {
-    user: state.User.userInfo,
-  }
-}
+    user: state.User.userInfo
+  };
+};
 
 const mapDispatchToProps = dispatch => ({
   getUser: user => dispatch(getUser(user))
