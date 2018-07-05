970189868e180eb6f395e297d02012ca0c5a0113
archon21 <rwetmore3@gmail.com>
2018-07-05 17:39:07 -0400

    edit changes

--- App/components/FutureProjections.js
+++ App/components/FutureProjections.js
@@ -1,12 +1,38 @@
-import React from 'react'
-import {View} from 'react-native'
+import React, { Component } from 'react';
+import { View, Button } from 'react-native';
+import Firebase from './Firebase/Firebase';
+import axios from 'axios';
+import styles from '../../public'
 
-const FutureProjections = props => {
-  return (
-    <View>
-      f
-    </View>
-  )
+class FutureProjections extends Component {
+  handlePrediction = async () => {
+    const result = await axios.post(
+          'https://safe-bastion-55889.herokuapp.com/api/loadKeras',
+          { lastThreeDays: [30, 22, 37] }
+        );
+    // Firebase.ref('/dummyData').once(async snapshot => {
+    //   const result = await axios.post(
+    //     'https://safe-bastion-55889.herokuapp.com/api/loadKeras',
+    //     { lastThreeDays: snapshot }
+    //   );
+    // });
+    console.log(result);
+  };
+  render() {
+    return (
+      <View>
+        <Button
+          onPress={() => {
+            this.handlePrediction();
+          }}
+          title="Give Me Prediction"
+          raised={true}
+          backgroundColor="#0080ff"
+          style={styles.signUpButton}
+        />
+      </View>
+    );
+  }
 }
 
-export default FutureProjections
+export default FutureProjections;
