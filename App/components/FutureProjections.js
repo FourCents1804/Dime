05ab66b6d41ba8017395f704faaad1273a0c13de
Yacine Benzerga <benzerga.yacine@hotmail.com>
2018-07-07 23:21:16 -0400

    feat(FutureProj minor fixes deleted keras.js and other packages)

--- App/components/FutureProjections.js
+++ App/components/FutureProjections.js
@@ -1,32 +1,25 @@
 import React, { Component } from 'react';
-import { View, Button, Text } from 'react-native';
+import { View, Text } from 'react-native';
 import Firebase from './Firebase/Firebase';
 import axios from 'axios';
-import styles from '../../public'
-import {
-  AnimatedGaugeProgress
-} from 'react-native-simple-gauge';
+import styles from '../../public';
+import { AnimatedGaugeProgress } from 'react-native-simple-gauge';
 
 class FutureProjections extends Component {
   handlePrediction = async () => {
-    const result = await axios.post(
-          'https://safe-bastion-55889.herokuapp.com/api/loadKeras',
-          { lastThreeDays: [30, 22, 37] }
-        );
-    // Firebase.ref('/dummyData').once(async snapshot => {
-    //   const result = await axios.post(
-    //     'https://safe-bastion-55889.herokuapp.com/api/loadKeras',
-    //     { lastThreeDays: snapshot }
-    //   );
-    // });
-    console.log(result);
+    await Firebase.ref('/dummyData').once(async snapshot => {
+      const result = await axios.get(
+        `https://safe-bastion-55889.herokuapp.com/api/loadKeras?lastThreeDays=${snapshot}`
+      );
+      console.log(result);
+    });
   };
 
   formatMoney = number => {
-    return number.toLocaleString(
-      'en-US',
-      { style: 'currency', currency: 'USD' }
-    );
+    return number.toLocaleString('en-US', {
+      style: 'currency',
+      currency: 'USD'
+    });
   };
 
   render() {
@@ -34,11 +27,12 @@ class FutureProjections extends Component {
     const width = 15;
     const cropDegree = 90;
     const textOffset = width;
-    const textWidth = size - (textOffset * 2);
-    const textHeight = size * (1 - cropDegree / 360) - (textOffset * 2);
-    const low = 18.53
-    const high = 24.77
-    const average = 21.65
+    const textWidth = size - textOffset * 2;
+    const textHeight = size * (1 - cropDegree / 360) - textOffset * 2;
+    const ErrorStd = 3.12;
+    const low = this.result - ErrorStd;
+    const high = this.result + ErrorStd;
+    const average = this.result;
 
     return (
       <View style={styles.futureProjectionsContainer}>
@@ -52,23 +46,31 @@ class FutureProjections extends Component {
           tintColor="#008ECC"
           backgroundColor="#b0c4de"
           stroke={[2, 2]}
-          strokeCap="circle">
-          <View style={{position: 'absolute',
-            top: textOffset,
-            left: textOffset,
-            width: textWidth,
-            height: textHeight,
-            alignItems: 'center',
-            justifyContent: 'center'}}>
+          strokeCap="circle"
+        >
+          <View
+            style={{
+              position: 'absolute',
+              top: textOffset,
+              left: textOffset,
+              width: textWidth,
+              height: textHeight,
+              alignItems: 'center',
+              justifyContent: 'center'
+            }}
+          >
             <Text style={styles.futureProjectionsText}>Projected Spend:</Text>
-            <Text style={styles.futureProjectionsTitle}>{this.formatMoney(average)}</Text>
+            <Text style={styles.futureProjectionsTitle}>
+              {this.formatMoney(average)}
+            </Text>
           </View>
         </AnimatedGaugeProgress>
         <Text style={styles.futureProjectionsText}>
-          Based on your spending habits, you are projected to spend between {this.formatMoney(low)} and {this.formatMoney(high)} today.
+          Based on your spending habits, you are projected to spend between{' '}
+          {this.formatMoney(low)} and {this.formatMoney(high)} today.
         </Text>
       </View>
-    )
+    );
   }
 }
 
